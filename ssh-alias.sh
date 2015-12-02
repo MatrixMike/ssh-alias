980a88fab030bc87661355448dfa5b792e52f1a3
Alexar <hi@alexar.me>
2015-12-02 16:41:34 +1100

    The first commit which works well enough\!

---
+++ ssh-alias.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+echo "This will setup keypair-authentication so you would not need password to connect to server anymore"
+
+# ssh_alias=""
+echo "SSH alias?"; read ssh_alias
+
+# ssh_user="root"
+echo "Username?"; read ssh_user
+
+# ssh_server=""
+echo "Server IP?"; read ssh_server
+
+# ssh_port="22"
+echo "Port?"; read ssh_port
+
+
+ssh_keyfile=$HOME"/.ssh/"${ssh_alias}"_rsa"
+echo -e "\n\n\n" | ssh-keygen -t rsa -f $ssh_keyfile -N ""
+
+if [ ! -f /usr/local/bin/ssh-copy-id ]; then
+	echo "Installing ssh-copy-id"
+	curl -L https://raw.githubusercontent.com/beautifulcode/ssh-copy-id-for-OSX/master/install.sh | sh
+fi
+
+echo "Installing the key on the server "$ssh_server
+ssh_string=$(echo $ssh_user"@"$ssh_server" -p"$ssh_port)
+ssh-copy-id -i $ssh_keyfile "$ssh_string"
+
+echo "Updating .ssh/config on your machine"
+echo "\n\n# An alias to connect to "$ssh_alias" server\nHost "$ssh_alias"\n\tHostname "$ssh_server"\n\tUser "$ssh_user"\n\tPort "$ssh_port"\n\tIdentityFile "$ssh_keyfile"\n\n" >> $HOME/.ssh/config
+
+echo "SSH into server using the alias"
+ssh $ssh_alias
